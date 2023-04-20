61ce9d3553f770da5135dc4d0a0ae9a0955fdb3c
KawsarCoder <96495318+KawsarCoder@users.noreply.github.com>
2023-04-21 01:29:06 +0600

    home banner updated

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import Banner from "./Banner/Banner";
 
 const Home = () => {
-    return (
-        <div className='mx-5'>
-            <h2>This home</h2>
-        </div>
-    );
+  return (
+    <div className="mx-5">
+      <Banner />
+    </div>
+  );
 };
 
 export default Home;
