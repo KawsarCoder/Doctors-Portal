dc408c34aff546b71309fef32801a3a07e3b0a03
KawsarCoder <96495318+KawsarCoder@users.noreply.github.com>
2023-04-25 00:37:52 +0600

    login form added

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,11 +1,63 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div>
-            <h1>This is login</h1>
-        </div>
-    );
+  const { register, handleSubmit } = useForm();
+  const handleLogin = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="h-[800px] flex justify-center items-center">
+      <div className="w-96 p-7">
+        <h2 className="text-xl text-center"> Login</h2>
+        <form onSubmit={handleSubmit(handleLogin)}>
+          {/* <Header /> */}
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+
+            <input
+              {...register("email")}
+              type="text"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+
+            <input
+              {...register("password")}
+              type="password"
+              className="input input-bordered w-full max-w-xs"
+            />
+            <label className="label">
+              <span className="label-text">Forget Password?</span>
+            </label>
+          </div>
+          <input
+            type="submit"
+            className="text-white btn btn-accent w-full"
+            value="login"
+          />
+        </form>
+        <p>
+          New to Doctor's Portal?{" "}
+          <Link className="text-secondary" to="/signup">
+            Please sign up
+          </Link>
+        </p>
+
+        <div className="divider">OR</div>
+        <button className="btn btn-outline w-full">CONTINUE WITH GOOGLE</button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
