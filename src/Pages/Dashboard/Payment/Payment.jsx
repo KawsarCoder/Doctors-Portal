51ca8b8bc16e424173e50aafd620eb6e8a6b5b04
KawsarCoder <96495318+KawsarCoder@users.noreply.github.com>
2023-05-15 11:46:41 +0600

    payment intent added

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,18 +1,28 @@
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import CheckoutForm from "./CheckOutForm/CheckoutForm";
 
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PK);
 const Payment = () => {
   const booking = useLoaderData();
 
-  const { treatment, price, appointmentDate, slots } = booking;
+  const { treatment, price, appointmentDate, slot } = booking;
 
   return (
     <div>
       <h3 className="text-3xl">Payment for {treatment}</h3>
       <p className="text-xl">
-        Please pay <strong className="text-orange-400">{price} </strong> for your
-        appointment on {appointmentDate} at {slots}
+        Please pay <strong className="text-orange-400">{price} </strong> for
+        your appointment on {appointmentDate} at {slot}
       </p>
+      <div className="w-96 my-12">
+        <Elements stripe={stripePromise}>
+          <CheckoutForm booking={booking} />
+        </Elements>
+      </div>
     </div>
   );
 };
